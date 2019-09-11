aa604b144c4a7e6e59c2845b3cf2aef80ee613b4
hedeesa <moghimi.saeedeh@gmail.com>
2019-09-11 15:40:00 +0430

    prettifies volcanoes popups

--- app.py
+++ app.py
@@ -6,15 +6,24 @@ map = folium.Map(location=[39, -100], zoom_start=5)
 fg = folium.FeatureGroup(name="My Map")
 
 # read from file
-
 file = pandas.read_csv("Volcanoes.txt")
 lat = file["LAT"]
 lon = file["LON"]
 name = file["NAME"]
+elev = file["ELEV"]
 
-for nme, lt, ln in zip(name, lat, lon):
+html = """<h4> %s </h4>
+Height: %s m
+<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a>
+"""
+
+for nme, lt, ln, el in zip(name, lat, lon, elev):
+    iframe = folium.IFrame(html=html % (
+        nme, str(el), nme, "More information"), width=200, height=100)
+
     fg.add_child(folium.Marker(location=[
-        lt, ln], popup=nme, icon=folium.Icon(color='red')))
+        lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 map.save("index.html")
