2dced757a966909caeb62f1a663bcdacea7c33ab
hedeesa <moghimi.saeedeh@gmail.com>
2019-09-11 15:22:46 +0430

    added volcanoes

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import folium
+import pandas
 
 map = folium.Map(location=[39, -100], zoom_start=5)
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[
-             38.2, -99.1], popup="Hi, I am a Marker", icon=folium.Icon(color='green')))
+
+# read from file
+
+file = pandas.read_csv("Volcanoes.txt")
+lat = file["LAT"]
+lon = file["LON"]
+name = file["NAME"]
+
+for nme, lt, ln in zip(name, lat, lon):
+    fg.add_child(folium.Marker(location=[
+        lt, ln], popup=nme, icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 map.save("index.html")
