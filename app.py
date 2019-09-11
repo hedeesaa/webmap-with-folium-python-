e98d3ef29090373d65c3bf3ee3aba778afdb98db
hedeesa <moghimi.saeedeh@gmail.com>
2019-09-11 15:04:45 +0430

    added a marker

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import folium
 
 map = folium.Map(location=[39, -100], zoom_start=5)
-map.save("Map1.html")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[
+             38.2, -99.1], popup="Hi, I am a Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+map.save("index.html")
