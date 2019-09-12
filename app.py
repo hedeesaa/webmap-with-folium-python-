1f8b2b32d7ae3a14d73bf6ccd9141cafc52ad05c
hedeesa <moghimi.saeedeh@gmail.com>
2019-09-12 15:42:13 +0430

    add population Layer

--- app.py
+++ app.py
@@ -35,5 +35,8 @@ for nme, lt, ln, el in zip(name, lat, lon, elev):
     fg.add_child(folium.CircleMarker(
         location=[lt, ln], radius=7, popup=folium.Popup(iframe), fill_color=color_height(el), color=color_height(el), fill_opacity=0.7))
 
+fg.add_child(folium.GeoJson(
+    data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 map.save("index.html")
